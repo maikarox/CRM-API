6771eafa6ed60c065edd92372164a9f5f3859b5f
mayrez <maikarox@protonmail.com>
2021-10-10 18:37:47 +0100

    Update seed DB data
    - Populate some customers with profileImage

--- scripts/db-seed.js
+++ scripts/db-seed.js
@@ -1,5 +1,6 @@
 const MongoClient = require('mongodb').MongoClient;
 const Types = require('mongoose').Types;
+const { profileImageBase64 } = require('./imageFile/profileImage');
 const dotenv = require('dotenv');
 
 dotenv.config();
@@ -13,10 +14,10 @@ async function seedDB() {
     await client.connect();
 
     console.log('Connected correctly to server');
-    
+
     const db = client.db(MONGODB_DATABASE);
     const rolesCollection = db.collection('roles');
-    
+
     await rolesCollection.deleteMany({});
     await rolesCollection.insertMany([
       {
@@ -37,11 +38,11 @@ async function seedDB() {
           'create:all_users',
           'update:all_users',
           'delete:all_users',
-          'update:all_roles'
+          'update:all_roles',
         ],
       },
     ]);
-    
+
     const usersCollection = db.collection('users');
     await usersCollection.deleteMany({});
     await usersCollection.insertMany([
@@ -76,86 +77,86 @@ async function seedDB() {
         roles: [new Types.ObjectId('616191c1fc13ae60130001e5')],
       },
     ]);
-    
+
+    const profileImageBinary = Buffer.from(profileImageBase64, 'base64');
     const customersCollection = db.collection('customers');
     await customersCollection.deleteMany({});
     await customersCollection.insertMany([
       {
-        _id: new Types.ObjectId("61618fa7fc13ae40f1000028"),
-        name: "Dulcine",
-        surname: "London",
-        profileImage: "dlondon0@shutterfly.com",
-        createdAt: "2021-01-03T08:33:25.000Z",
-        updatedAt: "2021-08-13T02:35:22.000Z",
-        updatedBy: new Types.ObjectId("61616e99fc13ae4d5f00012f"),
-        createdBy: new Types.ObjectId("61616e99fc13ae4d5f00012f"),
-        deletedAt: null
+        _id: new Types.ObjectId('61618fa7fc13ae40f1000028'),
+        name: 'Dulcine',
+        surname: 'London',
+        profileImage: profileImageBinary,
+        createdAt: '2021-01-03T08:33:25.000Z',
+        updatedAt: '2021-08-13T02:35:22.000Z',
+        updatedBy: new Types.ObjectId('61616e99fc13ae4d5f00012f'),
+        createdBy: new Types.ObjectId('61616e99fc13ae4d5f00012f'),
+        deletedAt: null,
       },
       {
-        _id: new Types.ObjectId("61618fa7fc13ae40f100002b"),
-        name: "Nana",
-        surname: "Brill",
-        profileImage: "nbrill1@moonfruit.com",
-        createdAt: "2021-08-28T14:33:29.000Z",
-        updatedAt: "2021-01-14T08:43:40.000Z",
-        updatedBy: new Types.ObjectId("61616e99fc13ae4d5f00012f"),
-        createdBy: new Types.ObjectId("61616e99fc13ae4d5f00012f"),
-        deletedAt: null
-      }, 
-      {
-        _id: new Types.ObjectId("61618fa7fc13ae40f100002e"),
-        name: "Marcelline",
-        surname: "Rockingham",
-        profileImage: "mrockingham2@ox.ac.uk",
-        createdAt: "2020-09-28T15:36:37.000Z",
-        updatedAt: "2021-08-09T05:42:06.000Z",
-        updatedBy: new Types.ObjectId("61616e99fc13ae4d5f00012f"),
-        createdBy: new Types.ObjectId("61616e99fc13ae4d5f00012f"),
-        deletedAt: null
-      }, 
-      {
-        _id: new Types.ObjectId("61618fa7fc13ae40f1000031"),
-        name: "Debra",
-        surname: "Drewry",
-        profileImage: "ddrewry3@jugem.jp",
-        createdAt: "2021-01-25T10:32:30.000Z",
-        updatedAt: "2021-06-24T13:31:35.000Z",
-        updatedBy: new Types.ObjectId("61616e99fc13ae4d5f00012f"),
-        createdBy: new Types.ObjectId("61616e99fc13ae4d5f00012f"),
-        deletedAt: null
+        _id: new Types.ObjectId('61618fa7fc13ae40f100002b'),
+        name: 'Nana',
+        surname: 'Brill',
+        profileImage: profileImageBinary,
+        createdAt: '2021-08-28T14:33:29.000Z',
+        updatedAt: '2021-01-14T08:43:40.000Z',
+        updatedBy: new Types.ObjectId('61616e99fc13ae4d5f00012f'),
+        createdBy: new Types.ObjectId('61616e99fc13ae4d5f00012f'),
+        deletedAt: null,
       },
       {
-        _id: new Types.ObjectId("61618fa7fc13ae40f1000034"),
-        name: "Christoper",
-        surname: "Shallcrass",
-        profileImage: "cshallcrass4@shutterfly.com",
-        createdAt: "2020-12-12T01:53:40.000Z",
-        updatedAt: "2021-07-31T06:09:44.000Z",
-        updatedBy: new Types.ObjectId("61616e99fc13ae4d5f00012f"),
-        createdBy: new Types.ObjectId("61616e99fc13ae4d5f00012f"),
-        deletedAt: null
-      }, 
+        _id: new Types.ObjectId('61618fa7fc13ae40f100002e'),
+        name: 'Marcelline',
+        surname: 'Rockingham',
+        profileImage: null,
+        createdAt: '2020-09-28T15:36:37.000Z',
+        updatedAt: '2021-08-09T05:42:06.000Z',
+        updatedBy: new Types.ObjectId('61616e99fc13ae4d5f00012f'),
+        createdBy: new Types.ObjectId('61616e99fc13ae4d5f00012f'),
+        deletedAt: null,
+      },
       {
-        _id: new Types.ObjectId("61618fa7fc13ae40f1000040"),
-        name: "Vinni",
-        surname: "Mendoza",
-        profileImage: "vmendoza8@unicef.org",
-        createdAt: "2020-11-08T04:02:09.000Z",
+        _id: new Types.ObjectId('61618fa7fc13ae40f1000031'),
+        name: 'Debra',
+        surname: 'Drewry',
+        profileImage: null,
+        createdAt: '2021-01-25T10:32:30.000Z',
+        updatedAt: '2021-06-24T13:31:35.000Z',
+        updatedBy: new Types.ObjectId('61616e99fc13ae4d5f00012f'),
+        createdBy: new Types.ObjectId('61616e99fc13ae4d5f00012f'),
+        deletedAt: null,
+      },
+      {
+        _id: new Types.ObjectId('61618fa7fc13ae40f1000034'),
+        name: 'Christoper',
+        surname: 'Shallcrass',
+        profileImage: null,
+        createdAt: '2020-12-12T01:53:40.000Z',
+        updatedAt: '2021-07-31T06:09:44.000Z',
+        updatedBy: new Types.ObjectId('61616e99fc13ae4d5f00012f'),
+        createdBy: new Types.ObjectId('61616e99fc13ae4d5f00012f'),
+        deletedAt: null,
+      },
+      {
+        _id: new Types.ObjectId('61618fa7fc13ae40f1000040'),
+        name: 'Vinni',
+        surname: 'Mendoza',
+        profileImage: null,
+        createdAt: '2020-11-08T04:02:09.000Z',
         updatedAt: null,
         updatedBy: null,
-        createdBy: new Types.ObjectId("61616e99fc13ae4d5f00012c"),
-        deletedAt: null
-      }, 
+        createdBy: new Types.ObjectId('61616e99fc13ae4d5f00012c'),
+        deletedAt: null,
+      },
       {
-        _id: new Types.ObjectId("61618fa7fc13ae40f1000043"),
-        name: "Gianni",
-        surname: "Backler",
-        profileImage: "gbackler9@dailymotion.com",
-        createdAt: "2021-04-27T19:09:01.000Z",
-        updatedAt: "2021-01-28T07:15:49.000Z",
-        updatedBy: new Types.ObjectId("61616e99fc13ae4d5f00012c"),
-        createdBy: new Types.ObjectId("61616e99fc13ae4d5f00012c"),
-        deletedAt: null
+        _id: new Types.ObjectId('61618fa7fc13ae40f1000043'),
+        name: 'Gianni',
+        surname: 'Backler',
+        createdAt: '2021-04-27T19:09:01.000Z',
+        updatedAt: '2021-01-28T07:15:49.000Z',
+        updatedBy: new Types.ObjectId('61616e99fc13ae4d5f00012c'),
+        createdBy: new Types.ObjectId('61616e99fc13ae4d5f00012c'),
+        deletedAt: null,
       },
     ]);
 
