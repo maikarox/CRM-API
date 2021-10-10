33669fb46a743dea3e6b0c05444bfba62e7f1cd8
mayrez <maikarox@protonmail.com>
2021-10-10 18:36:15 +0100

    Update Customer model
    - profileImage is now a Buffer
    - deletedAt has no default value

--- src/models/customer.ts
+++ src/models/customer.ts
@@ -1,13 +1,13 @@
-import { Document, Schema, model } from 'mongoose';
+import { Document, Schema, model, Types } from 'mongoose';
 
 export interface Customer extends Document {
   _id: Schema.Types.ObjectId;
   name: string;
   surname: string;
-  profileImage?: string;
-  createdBy: Schema.Types.ObjectId;
+  profileImage?: Buffer | string;
+  createdBy: Types.ObjectId;
   createdAt: Date;
-  updatedBy?: Schema.Types.ObjectId;
+  updatedBy?: Types.ObjectId;
   updatedAt?: Date;
   deletedAt?: Date;
 }
@@ -19,7 +19,7 @@ function updatedByRequired(): boolean {
 const customerSchema = new Schema<Customer>({
   name: { type: String, required: true },
   surname: { type: String, required: true },
-  profileImage: String,
+  profileImage: { type: Schema.Types.Buffer },
   createdAt: { type: Date, required: true, default: new Date() },
   createdBy: { type: Schema.Types.ObjectId, ref: 'User', required: true },
   updatedAt: { type: Date, default: new Date() },
@@ -28,7 +28,7 @@ const customerSchema = new Schema<Customer>({
     ref: 'User',
     required: updatedByRequired,
   },
-  deletedAt: { type: Date, default: new Date() }
+  deletedAt: { type: Date }
 }, { collection: 'customers' });
 
 export const CustomerModel = model<Customer>(
