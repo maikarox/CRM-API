c32bacb22e41956fad2fc76ebd1518fc3aa926af
mayrez <maikarox@protonmail.com>
2021-10-10 18:43:00 +0100

    Add service and cotroller method for customers endpoints

---
+++ src/controllers/Customer.controller.ts
@@ -0,0 +1,90 @@
+import { RequestHandler } from 'express';
+
+import {
+  createCustomerProfile,
+  getAllCustomers,
+  removeCustomer,
+  softDeleteCustomer,
+  updateCustomerProfile,
+} from '../services/Customer.service';
+
+export const getCustomers: RequestHandler = async (_req, res) => {
+  const customers = await getAllCustomers();
+
+  res.status(200).json({ customers });
+};
+
+export const createCustomer: RequestHandler = async (req, res) => {
+  const { name, surname, profileImage, actionUser } = req.body;
+
+  if (!name || !surname) {
+    return res.status(400).json({
+      message: 'Name and surname are required.',
+    });
+  }
+
+  try {
+    const customer = await createCustomerProfile({
+      name,
+      surname,
+      profileImage,
+      actionUser,
+    });
+
+    return res.status(201).json({ customer });
+  } catch (err) {
+    return res.status(500).json({
+      message: `Error creating customer: ${err.message}`,
+    });
+  }
+};
+
+export const updateCustomer: RequestHandler = async (req, res) => {
+  const { name, surname, profileImage, actionUser } = req.body;
+  const { customerId } = req.params;
+
+  if (!name || !surname || !profileImage) {
+    return res.status(400).json({
+      message:
+        'Nothing to update, send at least one: name, surname or profileImage.',
+    });
+  }
+
+  try {
+    const customer = await updateCustomerProfile({
+      customerId,
+      name,
+      surname,
+      profileImage,
+      actionUser,
+    });
+
+    return res.status(200).json({ customer });
+  } catch (err) {
+    return res.status(500).json(`Error updating customer: ${customerId}`);
+  }
+};
+
+export const disableCustomer: RequestHandler = async (req, res) => {
+  const { customerId } = req.params;
+
+  try {
+    const customer = await softDeleteCustomer(customerId);
+    return res.status(200).json({ customer });
+  } catch (err) {
+    return res.status(500).json(`Error soft-deleting customer: ${err.message}`);
+  }
+};
+
+export const deleteUser: RequestHandler = async (req, res) => {
+  const { customerId } = req.params;
+
+  try {
+    await removeCustomer(customerId);
+    return res.sendStatus(204);
+  } catch (err) {
+    return res
+      .status(500)
+      .json({ message: `Couldn't remove customer: ${err.message}` });
+  }
+};
