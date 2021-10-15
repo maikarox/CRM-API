81a9f4a3f3d01bdaca49b16e2a8883ea4c0001bc
mayrez <maikarox@protonmail.com>
2021-10-15 08:12:23 +0100

    Update use fixtures

--- src/routes/users/fixtures/users.ts
+++ src/routes/users/fixtures/users.ts
@@ -1,9 +1,13 @@
 import { Types, Schema } from 'mongoose';
 import { User } from '../../../models';
-import { roleId1 } from './roles';
+import { roleId1, roleId3 } from './roles';
 
 export const userFixtureId = new Types.ObjectId(
   '61616e10fc13ae4d5f000c32',
+) as unknown as Schema.Types.ObjectId;
+
+export const user2FixtureId = new Types.ObjectId(
+  '61616e10fc13ae4d5f000c66',
 ) as unknown as Schema.Types.ObjectId;
 
 export const userFixture: Partial<User> = {
@@ -13,4 +17,13 @@ export const userFixture: Partial<User> = {
   email: 'user.test1@example.com',
   password: 'ee250b9faa440d344ddebcb27abe9eaac87f8aca7b0584d95bc2cc3d94c9e250',
   roles: [roleId1],
+};
+
+export const user2Fixture: Partial<User> = {
+  _id: user2FixtureId,
+  name: 'Alice',
+  surname: 'Stone',
+  email: 'alice.stone@example.com',
+  password: 'ee250b9faa440d344ddebcb27abe9eaac87f8aca7b0584d95bc2cc3d94c9e250',
+  roles: [roleId3],
 };
