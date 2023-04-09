b5f96de993ac32097443fc63eed5262006f756e8
mayrez <maikarox@protonmail.com>
2023-04-09 11:32:48 +0100

    Add db methods in models

--- src/models/customer.ts
+++ src/models/customer.ts
@@ -1,10 +1,10 @@
-import { Document, Schema, model, Types } from 'mongoose';
+import { Document, Schema, model, Types, ObjectId, SortOrder, RootQuerySelector } from 'mongoose';
 
 export interface Customer extends Document {
   _id: Schema.Types.ObjectId;
   name: string;
   surname: string;
-  profileImage?: Buffer | string;
+  profileImage?: Schema.Types.Buffer | string;
   createdBy: Types.ObjectId;
   createdAt: Date;
   updatedBy?: Types.ObjectId;
@@ -35,3 +35,59 @@ export const CustomerModel = model<Customer>(
   'Customer',
   customerSchema,
 );
+
+export async function findCustomerById (id: ObjectId): Promise<Customer>{
+  return await CustomerModel.findById({_id: id});
+}
+
+export async function getAllCustomers(sortBy: string | { [key: string]: SortOrder }): Promise<Customer[]> {
+  return await CustomerModel.find({}).sort(sortBy);
+}
+
+export async function createCustomer(customer: RootQuerySelector<Customer>): Promise<Customer>{
+  const { name, surname, profileImage, createdAt, createdBy, updatedAt, updatedBy } = customer;
+ 
+  return await CustomerModel.create({
+    name,
+    surname,
+    profileImage,
+    createdAt,
+    createdBy,
+    updatedAt,
+    updatedBy,
+  })
+}
+
+export async function findCustomerAndUpdate(args: RootQuerySelector<Customer>): Promise<Customer> {
+  return await CustomerModel.findOneAndUpdate(
+    { _id: args._id },
+    {
+      $set: {
+        ...args,
+        updatedAt: new Date(),
+        updatedBy: args.actionUserId,
+        profileImage: args.profileImage
+      }
+    },
+    { new: true },
+  ).lean();
+}
+
+export async function softDeleteCustomer(id: string): Promise<Customer> {
+  const now = new Date();
+
+  return await CustomerModel.findOneAndUpdate(
+    { _id: id },
+    {
+      $set: {
+        deletedAt: now,
+        updatedAt: now,
+      },
+    },
+    { new: true },
+  ).lean();
+}
+
+export async function hasrdDeleteCustomer(id: string): Promise<Customer> {
+  return await CustomerModel.findByIdAndDelete({ _id: id });
+}
