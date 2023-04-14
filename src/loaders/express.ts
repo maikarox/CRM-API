7e4668a067fa76d32a7aa53488e5004a2db5cdc1
mayrez <maikarox@protonmail.com>
2023-04-14 21:50:15 +0100

    Express enabke trust proxy

--- src/loaders/express.ts
+++ src/loaders/express.ts
@@ -6,6 +6,7 @@ import routes from '../routes';
 
 export default (app: Application): void => {
   app.use(helmet());
+  app.enable('trust proxy');
   app.use(cors());
   app.use(express.json());
   app.use(express.urlencoded({ extended: false }));
